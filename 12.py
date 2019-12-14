5713f43cbe483a531500726596f79fbd0116b714
Oliver Johnston <oliver@ojohnston.com>
2019-12-14 17:23:52 +0000

    12 pt 2

--- 12.py
+++ 12.py
@@ -44,30 +44,63 @@ def step(moons):
         m.apply_velocity()
 
 
-moons = [
-    Moon(13, 9, 5),
-    Moon(8, 14, -2),
-    Moon(-5, 4, 11),
-    Moon(2, -6, 1)
-]
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+
+def get_moons():
+    return [
+        Moon(13, 9, 5),
+        Moon(8, 14, -2),
+        Moon(-5, 4, 11),
+        Moon(2, -6, 1)
+    ]
+
+
+def x_equal(a, b):
+    return a.position.x == b.position.x and a.velocity.x == b.velocity.x
+
+
+def y_equal(a, b):
+    return a.position.y == b.position.y and a.velocity.y == b.velocity.y
+
+
+def z_equal(a, b):
+    return a.position.z == b.position.z and a.velocity.z == b.velocity.z
+
+
+moons = get_moons()
 
 for i in range(0, 1000):
     step(moons)
 
 print(sum(m.get_energy() for m in moons))
 
-moons = [
-    Moon(13, 9, 5),
-    Moon(8, 14, -2),
-    Moon(-5, 4, 11),
-    Moon(2, -6, 1)
-]
+initial_moons = get_moons()
+moons = get_moons()
 
-energies = set([sum(m.get_energy() for m in moons)])
-while True:
+x_loop = 0
+y_loop = 0
+z_loop = 0
+
+step_num = 0
+while x_loop == 0 or y_loop == 0 or z_loop == 0:
     step(moons)
-    energy = sum(m.get_energy() for m in moons)
-    print(len(energies))
-    if energy in energies:
-        break
-    energies.add(energy)
+    step_num += 1
+
+    if x_loop == 0 and all(map(lambda i: x_equal(moons[i], initial_moons[i]), range(0, len(moons)))):
+        x_loop = step_num
+
+    if y_loop == 0 and all(map(lambda i: y_equal(moons[i], initial_moons[i]), range(0, len(moons)))):
+        y_loop = step_num
+
+    if z_loop == 0 and all(map(lambda i: z_equal(moons[i], initial_moons[i]), range(0, len(moons)))):
+        z_loop = step_num
+
+print(lcm(x_loop, lcm(y_loop, z_loop)))
