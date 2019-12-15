9a51c5cbb3711cf568394b00f3fedbc92daf4afc
Oliver Johnston <oliver@ojohnston.com>
2019-12-15 18:13:28 +0000

    remove random

--- 15.py
+++ 15.py
@@ -1,119 +1,131 @@
 import curses
 import intcode
-import math
 from time import sleep
 from curses import wrapper
-from random import random
+from enum import Enum
+
+
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    WEST = 3
+    EAST = 4
+
+
+class Location:
+    def __init__(self, x, y, distance_from_origin):
+        self.x = x
+        self.y = y
+        self.distance_from_origin = distance_from_origin
+        self.has_oxygen = False
+
+
+def turn_left(d):
+    if d == Direction.NORTH:
+        return Direction.WEST
+    if d == Direction.SOUTH:
+        return Direction.EAST
+    if d == Direction.WEST:
+        return Direction.SOUTH
+    return Direction.NORTH
+
+
+def turn_right(d):
+    if d == Direction.NORTH:
+        return Direction.EAST
+    if d == Direction.SOUTH:
+        return Direction.WEST
+    if d == Direction.WEST:
+        return Direction.NORTH
+    return Direction.SOUTH
 
 
 class Droid:
     def __init__(self, program, stdscr):
         self.program = program
-        self.direction = 1
-        self.found_oxygen = False
         self.flooding = False
-        self.distances = dict()
-        self.has_oxygen = dict()
-        self.walls = set()
 
+        self.stdscr = stdscr
+        self.win = curses.newwin(curses.LINES - 1, curses.COLS - 1, 0, 0)
         curses.noecho()
         curses.cbreak()
         curses.curs_set(False)
         stdscr.keypad(True)
 
-        self.stdscr = stdscr
-        self.win = curses.newwin(curses.LINES - 1, curses.COLS - 1, 0, 0)
-        self.x = curses.COLS // 2
-        self.y = curses.LINES // 2
+        origin = Location(curses.COLS // 2, curses.LINES // 2, 0)
+        self.locations = dict()
+        self.location = origin
+        self.locations[(origin.x, origin.y)] = origin
 
-        self.origin_x = self.x
-        self.origin_y = self.y
-        self.distances[(self.x, self.y)] = 0
-        self.has_oxygen[(self.x, self.y)] = False
-        self.repair_x = 0
-        self.repair_y = 0
-        self.win.addch(self.y, self.x, "D")
+        self.oxygen = None
+
+        self.desired_direction = Direction.NORTH
+        self.actual_direction = Direction.NORTH
 
     def store_output(self, o):
-        next_x = self.next_x()
-        next_y = self.next_y()
+        cur_location = self.location
+        next_location = self.next_location()
 
         if o == 0:
-            self.win.addch(next_y, next_x, "#")
-            self.walls.add((next_x, next_y))
+            # hit a wall
+            self.win.addch(next_location.y, next_location.x, "#")
+            if self.desired_direction == self.actual_direction:
+                # hit a wall when travelling in desired direction, turn fully left
+                self.actual_direction = turn_left(self.desired_direction)
+                self.desired_direction = self.actual_direction
+            else:
+                # hit a wall when testing a direction, turn back to the desired direction
+                self.actual_direction = self.desired_direction
         else:
-            self.win.addch(self.y, self.x, ".")
+            # moved forward
+            self.win.addch(cur_location.y, cur_location.x, ".")
+            self.location = next_location
+            self.locations[(next_location.x, next_location.y)] = next_location
 
-            distance = self.distances.get((self.x, self.y)) + 1
-            distance_next = self.distances.get((next_x, next_y), 999999)
+            next_location.distance_from_origin = min(next_location.distance_from_origin,
+                                                     cur_location.distance_from_origin + 1)
 
-            self.x = next_x
-            self.y = next_y
-
-            self.distances[(self.x, self.y)] = min(distance, distance_next)
-            self.has_oxygen[(self.x, self.y)] = False
+            # test moving right
+            self.desired_direction = self.actual_direction
+            self.actual_direction = turn_right(self.actual_direction)
 
         if o == 2:
-            self.repair_x = self.x
-            self.repair_y = self.y
-            self.has_oxygen[(self.x, self.y)] = True
-            self.found_oxygen = True
+            self.location.has_oxygen = True
+            self.oxygen = self.location
 
-        self.change_direction()
-
-        self.win.addch(self.y, self.x, "D")
-
-        if self.found_oxygen:
-            self.win.addch(self.repair_y, self.repair_x, "O")
-            self.win.addstr(0, 0, "Distance to oxygen: " + str(self.distances[(self.repair_x, self.repair_y)]))
+        if self.oxygen is not None:
+            self.win.addch(self.oxygen.y, self.oxygen.x, "O")
+            self.win.addstr(0, 0, "Distance to oxygen: " + str(self.oxygen.distance_from_origin))
             self.win.addstr(1, 0, "Press any key to start flooding with oxygen")
 
             self.win.nodelay(True)
             if self.win.getch() != curses.ERR:
                 self.flooding = True
 
+        self.win.addch(self.location.y, self.location.x, "D")
         self.win.refresh()
 
-    def next_x(self):
-        if self.direction == 3:
-            return self.x - 1
-        if self.direction == 4:
-            return self.x + 1
-        return self.x
+    def next_location(self):
+        x = self.location.x
+        y = self.location.y
 
-    def next_y(self):
-        if self.direction == 1:
-            return self.y - 1
-        if self.direction == 2:
-            return self.y + 1
-        return self.y
+        if self.actual_direction == Direction.NORTH:
+            y -= 1
+        elif self.actual_direction == Direction.SOUTH:
+            y += 1
+        elif self.actual_direction == Direction.WEST:
+            x -= 1
+        elif self.actual_direction == Direction.EAST:
+            x += 1
 
-    def change_direction(self):
-        up = (self.x, self.y - 1)
-        down = (self.x, self.y + 1)
-        west = (self.x - 1, self.y)
-        east = (self.x + 1, self.y)
-
-        direction = math.ceil(random()*4)
-        # if there is somewhere we haven't visited then go there, otherwise just random
-        # not perfect but if you wait long enough it works
-        for i in range(0, 4):
-            direction = (direction % 4) + 1
-            if direction == 1 and up not in self.walls and up not in self.has_oxygen:
-                break
-            if direction == 2 and down not in self.walls and down not in self.has_oxygen:
-                break
-            if direction == 3 and west not in self.walls and west not in self.has_oxygen:
-                break
-            if direction == 4 and east not in self.walls and east not in self.has_oxygen:
-                break
-
-        self.direction = direction
+        return self.locations.get((x, y), Location(x, y, 99999))
 
     def get_input(self):
         if self.flooding:
             return 99
-        return self.direction
+
+        sleep(0.003)
+        return self.actual_direction.value
 
     def run(self):
         intcode.execute_program(self.program, self.get_input, self.store_output)
@@ -122,24 +134,29 @@ class Droid:
 
     def flood(self):
         i = 0
-        while not all(self.has_oxygen.values()):
-            to_flood = list(filter(self.should_flood, self.has_oxygen.keys()))
-            for coord in to_flood:
-                self.has_oxygen[coord] = True
-                self.win.addch(coord[1], coord[0], "O")
-            self.win.refresh()
+        while not all(map(lambda x: x.has_oxygen, self.locations.values())):
+            to_flood = list(filter(self.should_flood, self.locations.values()))
+            for location in to_flood:
+                location.has_oxygen = True
+                self.win.addch(location.y, location.x, "O")
             i += 1
-            sleep(0.05)
             self.win.addstr(1, 0, "Flooding: " + str(i) + " minutes                        ")
+            self.win.refresh()
+            sleep(0.02)
 
         self.win.addstr(1, 0, "Flooded in " + str(i) + " minutes")
         self.win.refresh()
 
-    def should_flood(self, coord):
-        return (self.has_oxygen.get((coord[0] + 1, coord[1]))
-                or self.has_oxygen.get((coord[0], coord[1] + 1))
-                or self.has_oxygen.get((coord[0] - 1, coord[1]))
-                or self.has_oxygen.get((coord[0], coord[1] - 1)))
+    def should_flood(self, location):
+        n = self.locations.get((location.x, location.y - 1))
+        e = self.locations.get((location.x - 1, location.y))
+        s = self.locations.get((location.x, location.y + 1))
+        w = self.locations.get((location.x + 1, location.y))
+
+        return ((n is not None and n.has_oxygen)
+                or (s is not None and s.has_oxygen)
+                or (e is not None and e.has_oxygen)
+                or (w is not None and w.has_oxygen))
 
 
 def run(stdscr):
